bd84eb41147b78082e3223ae4cf300f67e13b84e
Bass <bass@Basss-MacBook-Air.local>
2022-12-14 10:07:02 +0700

    sdfoisj

--- src/views/staff/StaffSettings.js
+++ src/views/staff/StaffSettings.js
@@ -1,15 +1,23 @@
+import { get } from "components/Api";
 import { post } from "components/Api";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 // components
 
 export default function StaffSettings() {
   const [input, setInput] = useState({ oldPassword: '', newPassword: '', confirmPassword: '' })
-
+  const [detail, setDetail] = useState({})
   const inputChange = (event) => {
     const { name, value } = event.target
     setInput({
       ...input,
+      [name]: value
+    })
+  }
+  const detailChange = (event) => {
+    const { name, value } = event.target
+    setDetail({
+      ...detail,
       [name]: value
     })
   }
@@ -25,6 +33,30 @@ export default function StaffSettings() {
         }
       })
   }
+
+  const detailSubmitClick = (event) => {
+    event.preventDefault()
+    post(`${process.env.REACT_APP_API}/staff/updatedetail`, detail)
+      .then(result => {
+        if (!result.data.err) {
+          alert(result.data)
+        } else {
+          alert('ล้มเหลว')
+        }
+      })
+  }
+  useEffect(() => {
+    get(`${process.env.REACT_APP_API}/staff/setting`)
+      .then(result => {
+        console.log(result.data)
+        setDetail({
+          bank: result.data.detail[0]?.bank,
+          hq: result.data.detail[0]?.hq,
+          id: result.data.detail[0]?.id,
+          phone: result.data.detail[0]?.phone
+        })
+      })
+  }, [])
   return (
     <>
       <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
@@ -98,98 +130,101 @@ export default function StaffSettings() {
 
               </div>
             </div>
+          </form>
+        </div>
+      </div>
 
-            {/* <hr className="mt-6 border-b-1 border-blueGray-300" />
-
+      <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
+        <div className="rounded-t bg-white mb-0 px-6 py-6">
+          <div className="text-center flex justify-between">
+            <h6 className="text-blueGray-700 text-xl font-bold">Detail</h6>
+          </div>
+        </div>
+        <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+          <form onSubmit={detailSubmitClick}>
             <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
-              Contact Information
+              ข้อมูลส่วนตัว
             </h6>
             <div className="flex flex-wrap">
-              <div className="w-full lg:w-12/12 px-4">
+              <div className="w-full lg:w-6/12 px-4">
                 <div className="relative w-full mb-3">
                   <label
                     className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                     htmlFor="grid-password"
                   >
-                    Address
+                    รหัสประจำตัวประชาชน
                   </label>
                   <input
+                    name='id'
+                    value={detail.id}
                     type="text"
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                    onChange={detailChange}
                   />
                 </div>
               </div>
-              <div className="w-full lg:w-4/12 px-4">
+              <div className="w-full lg:w-6/12 px-4">
                 <div className="relative w-full mb-3">
                   <label
                     className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                     htmlFor="grid-password"
                   >
-                    City
+                    โทรศัพท์
                   </label>
                   <input
-                    type="email"
+                    name='phone'
+                    type="phone"
+                    value={detail.phone}
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue="New York"
+                    onChange={detailChange}
                   />
                 </div>
               </div>
-              <div className="w-full lg:w-4/12 px-4">
+              <div className="w-full lg:w-6/12 px-4">
                 <div className="relative w-full mb-3">
                   <label
                     className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                     htmlFor="grid-password"
                   >
-                    Country
+                    เลขบัญชี
                   </label>
                   <input
+                    name="bank"
                     type="text"
+                    value={detail.bank}
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue="United States"
+                    onChange={detailChange}
                   />
                 </div>
+
+                <button
+                  className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                  type="submit"
+                >
+                  Update
+                </button>
+
               </div>
-              <div className="w-full lg:w-4/12 px-4">
+
+              <div className="w-full lg:w-6/12 px-4">
                 <div className="relative w-full mb-3">
                   <label
                     className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                     htmlFor="grid-password"
                   >
-                    Postal Code
+                    สาขา
                   </label>
                   <input
-                    type="text"
+                    name='hq'
+                    type="number"
+                    min={0}
+                    value={detail.hq}
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue="Postal Code"
+                    onChange={detailChange}
                   />
                 </div>
               </div>
             </div>
-
-            <hr className="mt-6 border-b-1 border-blueGray-300" />
-
-            <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
-              About Me
-            </h6>
-            <div className="flex flex-wrap">
-              <div className="w-full lg:w-12/12 px-4">
-                <div className="relative w-full mb-3">
-                  <label
-                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                    htmlFor="grid-password"
-                  >
-                    About me
-                  </label>
-                  <textarea
-                    type="text"
-                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue="A beautiful UI Kit and Admin for React & Tailwind CSS. It is Free and Open Source."
-                    rows="4"
-                  ></textarea>
-                </div>
-              </div>
-            </div> */}
           </form>
         </div>
       </div>
